5f324c119ff0125d2bfd366ab20d8668ab937cc8
Aleksandr Myaots <amyaots@gmail.com>
2022-11-02 14:58:11 +0300

    add macCatalyst to platforms

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CWebRTC",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v12), .macCatalyst(.v13)],
     products: [
         .library(
             name: "CWebRTC",
