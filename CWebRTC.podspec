ada82d7d6adf44a85f3e9e81c51cb9fe1afbdc13
Anna Lemekhova <alemekhova@inno.tech>
2025-02-21 19:28:00 +0300

    Release/133.0.0
    Based on release 133 (with rebased patchset)

--- CWebRTC.podspec
+++ CWebRTC.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
 
-  version = '129.0.0'
+  version = '133.0.0'
 
   spec.name         = 'CWebRTC'
   spec.version      = version
--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let version = "129.0.0"
-let checksum = "0766bc0a20ceb2f585a268551ed812cdf9c8ad8f8bfa12a886fddbb72db316d3"
+let version = "133.0.0"
+let checksum = "1775049323ffa2a1587f930def727b75850049d60e761323e17e15d42c102b2a"
 
 let package = Package(
     name: "CWebRTC",
