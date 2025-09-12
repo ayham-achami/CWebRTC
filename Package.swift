076b84d94f29d3cf9938847f11c22428835e4b8d
annalem <Anna.Lemehova@gmail.com>
2025-09-12 09:16:18 +0300

    Release/138.0.0 (#19)
    Based on release 138 (with rebased patchset)
    
    Co-authored-by: Anna Lemekhova <alemekhova@inno.tech>

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let version = "133.0.3"
-let checksum = "3e8708f185d55ddb519ea20f0d3ade72a54c0045c3fca7cb2b058246834f2ee2"
+let version = "138.0.0"
+let checksum = "15b865a1a8abe4424b3c5c0a058aa6194ebafe8e43a170d042886edf692e5cfc"
 
 let package = Package(
     name: "CWebRTC",
@@ -16,7 +16,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "WebRTC",
-                      url: "https://github.com/ayham-achami/CWebRTC/releases/download/\(version)/WebRTC.xcframework.zip?v=1",
+                      url: "https://github.com/ayham-achami/CWebRTC/releases/download/\(version)/WebRTC.xcframework.zip",
                       checksum: checksum)
     ]
 )
